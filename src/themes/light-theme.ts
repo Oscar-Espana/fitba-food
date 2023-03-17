fa2feca8e2e1ed4101028287e9eee5c8be0ec922
Oscar Espana <espaaoscar@gmail.com>
2023-03-16 22:49:00 -0500

    feat: setting up the light theme

--- src/themes/light-theme.ts
+++ src/themes/light-theme.ts
@@ -2,7 +2,45 @@ import { createTheme, ThemeOptions } from "@mui/material/styles";
 
 const lightThemeOptions: ThemeOptions = {
   typography: {
-    fontFamily: ["var(--Caudex)", "sans-serif", "Roboto"].join(","),
+    fontFamily: ["var(--Poppins)", "sans-serif", "Roboto"].join(","),
+    h1: {
+      fontFamily: "var(--Dancing_Script)",
+      fontSize: "88px",
+      fontWeight: 400,
+      color: "#F2B410",
+    },
+    h2: {
+      fontFamily: "var(--Dancing_Script)",
+      fontSize: "72px",
+      fontWeight: 700,
+      color: "#F2B410",
+      lineHeight: 0.95,
+    },
+    h3: {
+      fontFamily: "var(--Poppins)",
+      fontSize: "52px",
+      fontWeight: 600,
+    },
+    subtitle1: {
+      fontFamily: "var(--Poppins)",
+      fontSize: "24px",
+      fontWeight: 500,
+    },
+    subtitle2: {
+      fontFamily: "var(--Poppins)",
+      fontSize: "22px",
+      fontWeight: 500,
+    },
+    body1: {
+      fontFamily: "var(--Poppins)",
+      fontSize: "19px",
+      fontWeight: 400,
+    },
+    body2: {
+      fontFamily: "var(--Poppins)",
+      fontSize: "17px",
+      fontWeight: 400,
+    },
   },
 };
 
