1ef8a65bd100d634a98023fccd4307cc44fac3e8
Oscar Espana <espaaoscar@gmail.com>
2023-03-16 23:56:04 -0500

    feat: building the Phrase ssection

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "./sections/Hero";
 import MenuProducts from "./sections/MenuProducts";
+import Phrase from "./sections/Phrase";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Phrase />
       <MenuProducts />
     </>
   );
---
+++ src/screens/Home/sections/Phrase.tsx
@@ -0,0 +1,63 @@
+import { Box, Grid, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import Image from "next/image";
+import React from "react";
+
+const Phrase = () => {
+  return (
+    <Box
+      component={"section"}
+      sx={{
+        py: 13,
+        pb: 0,
+      }}
+    >
+      <Container>
+        <Grid
+          container
+          spacing={{
+            xs: 2,
+            md: 4,
+            lg: 8,
+          }}
+        >
+          <Grid item xs={12} sm={4}>
+            <Typography
+              sx={{
+                fontSize: "32px",
+                letterSpacing: "-0.5px",
+              }}
+            >
+              {`"Que la comida sea tu medicina y la medicina sea tu comida."`}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={8}>
+            <Box
+              component="picture"
+              sx={{
+                display: "block",
+                position: "relative",
+                width: "100%",
+                height: "450px",
+                overflow: "hidden",
+                borderRadius: "20px",
+              }}
+            >
+              <Image
+                className="healty-food"
+                src="/images/comida-saludable.jpg"
+                alt="pizzas"
+                fill
+                style={{
+                  objectFit: "cover",
+                }}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+
+export default Phrase;
