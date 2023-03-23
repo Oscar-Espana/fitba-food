c77a61dfd3f10c21b23a4005aac98067c5fb59af
Oscar Espana <espaaoscar@gmail.com>
2023-03-22 23:36:08 -0500

    feat: add basic SEO for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import Home from "@/screens/Home";
+import Head from "next/head";
 
 const HomePage = () => {
-  return <Home />;
+  return (
+    <>
+      <Head>
+        <title>FitbaFood</title>
+        <meta
+          name="description"
+          content="Fitbafood: Alimentarse de forma saludable nunca fue tan delicioso"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/icons/logo-fitba-food.svg" />
+      </Head>
+      <Home />
+    </>
+  );
 };
 
 export default HomePage;
